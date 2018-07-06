0acc594bc616660de1b49fc3a0bc39b42bcdd918
abhinavreddy98 <rddyabhinav@gmail.com>
2018-07-06 12:00:43 +0530

    Update JoinGrafana.sh

--- JoinGrafana.sh
+++ JoinGrafana.sh
@@ -2,5 +2,6 @@
 wget https://gallery.azure.com/artifact/20151001/grafana-labs.grafana_ossstable.1.0.2/Artifacts/scripts/install-grafana.sh
 sudo bash install-grafana.sh $6
 sudo bash setup-grafana-vm.sh $1 $2 $3 $4 $5
+grafana-cli plugins install grafana-azure-monitor-datasource
 sudo service grafana-server restart
 sudo bash install-kusto-ds.sh
